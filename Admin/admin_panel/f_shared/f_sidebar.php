c09ada2a924e9d0db942b3202578f830ad03d1b6
Syed Ikrama <121003958+syedikrama@users.noreply.github.com>
2025-01-11 11:05:32 +0500

    add all files

--- Admin/admin_panel/f_shared/f_sidebar.php
+++ Admin/admin_panel/f_shared/f_sidebar.php
@@ -3,7 +3,7 @@
         <div class="sidebar-logo">
           <!-- Logo Header -->
           <div class="logo-header" data-background-color="dark">
-            <a href="index.php" class="logo">
+            <a href="../index.php" class="logo">
               <img
                 src="../assets/img/kaiadmin/logo_light.svg"
                 alt="navbar brand"
